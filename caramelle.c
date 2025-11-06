1a8bd84b57df89acf05d604f27dbcb648182137e
battledarkal <steamjacopo31@gmail.com>
2025-11-06 17:53:50 +0000

    es5

--- caramelle.c
+++ caramelle.c
@@ -1,1 +1,17 @@
-<>;
+#include<stdio.h>
+
+int main() {
+    int caramelle, persone, testa, non;
+    printf("quante caramelle ci sono e quante persone ci sono: ");
+    scanf("%d", &caramelle);
+    scanf("%d", &persone);
+
+    testa = persone/caramelle;
+    non = caramelle - (testa * persone);
+
+    printf("le caramelle a testa sono %d\n", testa);
+    printf("le caramelle in più sono %d", non);
+
+    return 0;
+
+}
