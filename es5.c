900dda58a45bdaa82235a86f4e073dfafd7c79db
battledarkal <steamjacopo31@gmail.com>
2025-11-06 17:58:00 +0000

    dopo esercizio 5

---
+++ es5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+   
+    int a, b, c;
+
+    printf("inserisci le tre variabili a, b, c:  ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+// non so come fare delle variabili temporanee per scambiare i valori-
+
+    return 0;
+}
