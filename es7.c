e92e7841b5c6a5fdc7ceea56064dba39254e8dc1
battledarkal <steamjacopo31@gmail.com>
2025-11-21 11:41:06 +0100

    Update es7.c

--- es7.c
+++ es7.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main () {
+
+  char lettera, grande;
+printf("inserisci una lettera dell'alfabeto minuscola:  ");
+scanf("%c", &lettera);
+printf("Il carattere inserito è %d (ascii %d)\n", lettera, lettera);
+grande = lettera - 20;
+printf("la lettera maiuscola è: %c\n", grande);
+}
