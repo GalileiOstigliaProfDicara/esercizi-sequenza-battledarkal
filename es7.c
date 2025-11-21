0447766d5e170274d4584da66a7b2d32cc2a29fd
battledarkal <steamjacopo31@gmail.com>
2025-11-21 11:44:02 +0100

    fine es7.c

--- es7.c
+++ es7.c
@@ -6,6 +6,6 @@ int main () {
 printf("inserisci una lettera dell'alfabeto minuscola:  ");
 scanf("%c", &lettera);
 printf("Il carattere inserito è %d (ascii %d)\n", lettera, lettera);
-grande = lettera - 20;
+grande = lettera - 32;
 printf("la lettera maiuscola è: %c\n", grande);
 }
